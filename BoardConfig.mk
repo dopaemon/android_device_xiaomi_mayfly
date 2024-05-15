51a80d5112c76a0a8a9413096a62ea17e69b2ef7
fiirefly24 <muhammadalifikri2412@gmail.com>
2024-05-15 13:57:38 +0000

    mayfly: Ship with MIUI Camera
    *Device      : https://github.com/cupid-development/android_device_xiaomi_miuicamera-cupid
    *Vendor      : https://git.mainlining.org/cupid-development/proprietary_vendor_xiaomi_miuicamera-cupid
    
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,9 @@
 
 DEVICE_PATH := device/xiaomi/mayfly
 
+# Miui Camera for cupid
+include device/xiaomi/miuicamera-cupid/BoardConfig.mk
+
 # Inherit from sm8450-common
 include device/xiaomi/sm8450-common/BoardConfigCommon.mk
 
