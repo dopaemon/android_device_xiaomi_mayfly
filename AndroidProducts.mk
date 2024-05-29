3d6beaba65451c3eec52940078cbf20070761c14
dopaemon <polarisdp@gmail.com>
2024-05-29 20:50:22 +0000

    VoltageOS
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_mayfly.mk
+    $(LOCAL_DIR)/voltage_mayfly.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_mayfly-user \
-    lineage_mayfly-userdebug \
-    lineage_mayfly-eng
+    voltage_mayfly-user \
+    voltage_mayfly-userdebug \
+    voltage_mayfly-eng
--- voltage_mayfly.mk
+++ voltage_mayfly.mk
@@ -8,20 +8,28 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common VoltageOS stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Inherit from mayfly device
 $(call inherit-product, device/xiaomi/mayfly/device.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+VOLTAGE_BUILD_TYPE := UNOFFICIAL
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := mayfly
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2206123SC
-PRODUCT_NAME := lineage_mayfly
+PRODUCT_NAME := voltage_mayfly
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="mayfly_cn-user 14 UKQ1.231003.002 V816.0.5.0.ULTCNXM release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/mayfly_cn/mayfly:14/UKQ1.231003.002/V816.0.5.0.ULTCNXM:user/release-keys
 
 # Miui Camera for mayfly
 $(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
