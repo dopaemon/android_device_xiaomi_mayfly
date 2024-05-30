2f61ae58f990e7babbb74e1741355c247e095abb
dopaemon <polarisdp@gmail.com>
2024-05-30 16:48:25 +0000

    AwakenOS
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_mayfly.mk
+    $(LOCAL_DIR)/awaken_mayfly.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_mayfly-user \
-    lineage_mayfly-userdebug \
-    lineage_mayfly-eng
+    awaken_mayfly-user \
+    awaken_mayfly-userdebug \
+    awaken_mayfly-eng
--- awaken_mayfly.mk
+++ awaken_mayfly.mk
@@ -8,18 +8,45 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common AwakenOS stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 
 # Inherit from mayfly device
 $(call inherit-product, device/xiaomi/mayfly/device.mk)
+
+# Boot Animation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Google Recorder
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# Google Assistant
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+
+# Pixel Charger
+USE_PIXEL_CHARGER := true
+
+# UnOfficial
+AWAKEN_BUILD_TYPE := UNOFFICIAL
+
+# Face unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Gapps
+WITH_GMS := true
+
+# Other
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_HAS_UDFPS := true
+TARGET_USE_PIXEL_FINGERPRINT := true
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := mayfly
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2206123SC
-PRODUCT_NAME := lineage_mayfly
+PRODUCT_NAME := awaken_mayfly
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
