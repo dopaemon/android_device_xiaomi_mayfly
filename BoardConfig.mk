bca53964a241cd197a3abe84fa9e62a10a72ea2a
dopaemon <polarisdp@gmail.com>
2025-10-02 08:31:17 +0000

    mayfly: MiuiCamera need Include before sm8450 common
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ DEVICE_PATH := device/xiaomi/mayfly
 # Inherit from sm8450-common
 include device/xiaomi/sm8450-common/BoardConfigCommon.mk
 
+# Miui Camera for cupid
+include device/xiaomi/miuicamera-cupid/BoardConfig.mk
+
 # Audio
 AUDIO_FEATURE_ENABLED_ULTRASOUND_PROXIMITY := true
 
