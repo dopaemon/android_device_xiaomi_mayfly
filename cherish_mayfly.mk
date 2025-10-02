22781aaa3932080fa223f314e75321354c695e65
dopaemon <polarisdp@gmail.com>
2025-10-02 11:27:01 +0000

    mayfly: BringUp CherishOS
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- cherish_mayfly.mk
+++ cherish_mayfly.mk
@@ -9,16 +9,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Inherit from mayfly device
 $(call inherit-product, device/xiaomi/mayfly/device.mk)
+
+# CherishOS
+WITH_GMS := true
+TARGET_USES_PICO_GAPPS := true
+TARGET_HAS_UDFPS := true
+CHERISH_BUILD_TYPE := UNOFFICIAL
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.cherish.maintainer=KernelPanix
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := mayfly
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2206123SC
-PRODUCT_NAME := lineage_mayfly
+PRODUCT_NAME := cherish_mayfly
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="mayfly-user 15 AQ3A.241006.001 OS2.0.204.0.VLTCNXM release-keys" \
