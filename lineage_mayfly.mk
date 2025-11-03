8ad2f3d43c73b25bbe5bedd732a19f72a9578703
dopaemon <polarisdp@gmail.com>
2025-11-03 01:08:08 +0000

    mayfly: Add Device Infomation
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- lineage_mayfly.mk
+++ lineage_mayfly.mk
@@ -23,6 +23,16 @@ TARGET_SUPPORTS_QUICK_TAP := true
 BYPASS_CHARGE_SUPPORTED := false
 TARGET_DEFAULT_PIXEL_LAUNCHER := true
 
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.mist.display=1080 x 2400, 120 hz
+    ro.mist.battery=4500mah
+    ro.mist.soc=Snapdragon® 8+ Gen 1
+    ro.mist.camera=50MP + 13MP + 5MP
+    ro.mist.front=32MP
+    ro.mist.platform=SM8450
+    ro.mist.screen=6.2' AMOLED
+    ro.mist.device.name=Xiaomi 12S
+
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := mayfly
 PRODUCT_MANUFACTURER := Xiaomi
